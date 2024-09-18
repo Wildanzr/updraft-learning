7ea39cfd111ead9d50f54f0f3c6d3a1ab60c7aab
Danzrrr <graita.sukma@gmail.com>
2024-09-18 23:44:25 +0700

    feat: deploy on zksync

---
+++ script/Deployer.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract Deployer is Script {
+    function run() external {
+        uint256 deployerPK = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPK);
+
+        new SimpleStorage();
+        vm.stopBroadcast();
+    }
+}
+
+/*
+How to run:
+    source .env
+    forge script --chain sepolia script/Deployer.s.sol:Deployer --rpc-url $BASE_SEPOLIA_RPC_URL --broadcast --verify -vvvv
+
+*/
