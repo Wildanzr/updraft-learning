6733a20c7d4e82499ce9c7985784a78f4aeb5710
Danzrrr <graita.sukma@gmail.com>
2024-09-30 21:42:51 +0700

    feat: add remappings

--- src/Meow.sol
+++ src/Meow.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.20;
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Meow {
+contract Meow is Ownable {
     uint256 meowCount;
+
+    constructor() Ownable(msg.sender) {}
 
     function meow() public returns (string memory) {
         meowCount++;
-        return "Meow miaww!";
+        return "Meow miaww moeewww";
     }
 }
